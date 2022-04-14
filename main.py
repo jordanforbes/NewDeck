2cbf479c45f1e97b3a7a9faab3256d506943c1c0
jordanforbes <jordforb@gmail.com>
2022-04-14 08:34:30 -0400

    changed 0 to 52

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import numpy.random as npr 
+import numpy as np 
 
 def p(x):
     print(x)
 
+#deck creation/shuffle 
 deck = npr.choice(52,52, replace=False) #0 = 52
 p(deck)
-npr.shuffle(deck)
+deck = np.where(deck == 0,52,deck)
+
 p(deck)
 
