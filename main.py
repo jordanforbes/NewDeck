990479e05a26aeccee9a4a3cae7ab09e61c55641
jordanforbes <jordforb@gmail.com>
2022-04-14 21:29:19 -0400

    deck class and methods

--- main.py
+++ main.py
@@ -5,64 +5,92 @@ def p(x=""):
     print(x)
 
 #deck creation/shuffle 
-deck = npr.choice(52,52, replace=False) #0 = 52
-hand = []
-# p(deck)
-deck = np.where(deck == 0,52,deck)
-card = 0
+class Deck():
+    deck = npr.choice(52, 52, replace=False)  # 0 = 52
+    deck = np.where(deck == 0, 52, deck)
+    hand = []
 
-p(deck)
+    def __init__(self):
+        p("$$$ Building Deck $$$")
+        self.deck = self.deck
+        self.showDeck()
+
 # p(deck.item(0))
-
-def draw(d=deck, h=hand):
-    card = deck[0]
-    h.append(card)
-    idCard(card)
-    d = d[1:]
-    p(d)
-    p()
-    p("in hand")
-    p(h)
-
-def idCard(c):
-    # p(c)
-    v=0
-    suit = ""
-
-    if c <=13:
-        v = c
-        suit = "Hearts"
-        
-    elif 13 < c <= 26:
-        v = c-13
-        suit = "Diamonds"
-
-    elif 26 < c <= 39:
-        v = c-26
-        suit = "Spades"
-
-    elif 39 < c: 
-        v = c- 39
-        suit = "Clubs"
-    name = checkFace(v)
-    p(f"{name} of {suit}")  
-    return (name,suit,c)
+    def draw(self):
+        p("$$$ Drawing Card $$$")
+        c = self.deck[0]
+        self.addToHand(c)
+        self.idCard(c)
+        self.remDeck()
+        p()
+        self.showHand()
+        p()
+        self.showDeck()
 
 
-def checkFace(n):
-    if n == 1:
-        return "Ace"
-    elif n == 11:
-        return "Jack"
-    elif n == 12:
-        return "Queen"
-    elif n == 13:
-        return "King"
-    else: 
-        return n
+    def idCard(self,c):
+        # p(c)
+        v=0
+        suit = ""
 
-draw()
+        if c <=13:
+            v = c
+            suit = "Hearts"
+            
+        elif 13 < c <= 26:
+            v = c-13
+            suit = "Diamonds"
 
+        elif 26 < c <= 39:
+            v = c-26
+            suit = "Spades"
+
+        elif 39 < c: 
+            v = c- 39
+            suit = "Clubs"
+        name = self.checkFace(v)
+        p(f"{name} of {suit}")  
+        return (name,suit,c)
+
+    def checkFace(self,n):
+        if n == 1:
+            return "Ace"
+        elif n == 11:
+            return "Jack"
+        elif n == 12:
+            return "Queen"
+        elif n == 13:
+            return "King"
+        else: 
+            return n
+
+    def addToHand(self,c):
+        p("XXX add to hand XXX")
+        self.hand.append(c)
+
+    def remDeck(self,times=1):
+        p("XXX remove from deck XXX")
+        self.deck= self.deck[times:]
+    
+    def remHand(self,times=1):
+        p("XXX remove from hand XXX")
+        self.hand= self.hand[times:]
+    
+    def showDeck(self):
+        p("in deck:")
+        p(self.deck)
+        return self.deck
+
+    def showHand(self):
+        p("in hand:")
+        p(self.hand)
+        return self.hand
+
+
+d = Deck()
+
+for x in range(5):
+    d.draw()
 
 # cTest = [1,4,9,13,14,26,32,39,21,27,39,40]
 # for x in cTest:
